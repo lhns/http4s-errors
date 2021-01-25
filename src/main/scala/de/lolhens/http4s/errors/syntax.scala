bd542047f6d2447b5e8b71905050223c9776d75e
Pierre Kisters <pierrekisters@gmail.com>
2021-01-25 15:58:50 +0100

    fixed errors

--- src/main/scala/de/lolhens/http4s/errors/syntax.scala
+++ src/main/scala/de/lolhens/http4s/errors/syntax.scala
@@ -8,23 +8,25 @@ import org.http4s.Response
 import org.http4s.dsl.impl.EntityResponseGenerator
 
 import java.io.{PrintWriter, StringWriter}
-import scala.util.chaining._
+import scala.reflect.ClassTag
 import scala.util.control.NonFatal
 
 object syntax {
 
   implicit class EitherTResponseOps[F[_], A](val eitherT: EitherT[F, Response[F], A]) extends AnyVal {
     def orError(implicit BracketThrowF: BracketThrow[F]): F[A] =
-      eitherT.leftSemiflatMap(response => BracketThrowF.raiseError(ResponseException(response))).merge
+      eitherT.leftSemiflatMap[A](response => BracketThrowF.raiseError(ResponseException[F](response))).merge
   }
 
   implicit def eitherTResponseNothingOps[F[_]](eitherT: EitherT[F, Response[F], Nothing]): EitherTResponseOps[F, Nothing] =
     new EitherTResponseOps[F, Nothing](eitherT)
 
   implicit class ResponseOps[F[_], A](val f: F[A]) extends AnyVal {
-    def orStacktrace(implicit BracketThrowF: BracketThrow[F]): EitherT[F, Either[Response[F], String], A] =
+    def orStacktrace(implicit BracketThrowF: BracketThrow[F]): EitherT[F, Either[Response[F], String], A] = {
+      val ResponseF = implicitly[ClassTag[Response[F]]]
+
       EitherT(f.attempt).leftMap {
-        case ResponseException(response: Response[F]) =>
+        case ResponseException(ResponseF(response)) =>
           Either.left(response)
 
         case NonFatal(throwable) =>
@@ -34,11 +36,15 @@ object syntax {
 
         case throwable => throw throwable
       }
+    }
 
     def orErrorResponse(status: EntityResponseGenerator[F, F],
                         log: String => Unit = System.err.println)
                        (implicit SyncF: Sync[F]): EitherT[F, Response[F], A] =
-      f.orStacktrace.leftSemiflatMap(_.fold(SyncF.delay, _.tap(log).pipe(status(_))))
+      f.orStacktrace.leftSemiflatMap(_.fold(SyncF.delay[Response[F]](_), { stacktrace =>
+        log(stacktrace)
+        status(stacktrace)
+      }))
   }
 
 }
